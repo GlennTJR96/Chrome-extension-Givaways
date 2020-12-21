33b20882e7b18f41c8c634d930b91affc091be51
GlennTan <e0310963@u.nus.edu>
2020-12-22 01:28:54 +0800

    First commit for this chrome extension!

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Button } from "./components/Button/Button";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello there! This is glenn</p>
+      </header>
+
+      <Button />
+    </div>
+  );
+};
+
+export default App;
